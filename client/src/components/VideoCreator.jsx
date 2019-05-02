afd96fdf41cd31cf8c5b6f3617da39df94d516ee
Casey Shamey <caseyshamey@gmail.com>
2019-05-02 12:45:11 -0400

    Complete video creater layout

--- client/src/components/VideoCreator.jsx
+++ client/src/components/VideoCreator.jsx
@@ -1,104 +1,36 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import classNames from 'classnames';
-// import { withStyles } from '@material-ui/core/styles';
+import Axios from 'axios';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { borders } from '@material-ui/system';
 
-// import Node from './helpers.js';
-
-// var node = new Node();
-// var currentNode = node;
 class VideoCreator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentNode: null,
-      parentNode: null
+      parentNode: null,
+      choiceOne: null,
+      choiceTwo: null
     }
-    // this.state = {
-    //   nodeTotal: node,
-    //   currentNode: node,
-    //   parentNode: currentNode.parent,
-    //   childOne: currentNode.children[0],
-    //   childTwo: currentNode.children[1],
-    //   choiceOne: '',
-    //   choiceTwo: '',
-    //   link: ''
-    // }
-
-    this.handleChoiceOneChange = this.handleChoiceOneChange.bind(this);
-    this.handleChoiceTwoChange = this.handleChoiceTwoChange.bind(this);
-    this.handleLinkChange = this.handleLinkChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.onGoBack = this.onGoBack.bind(this);
-    this.onNewNodeChoiceOne = this.onNewNodeChoiceOne.bind(this);
-    this.onNewNodeChoiceTwo = this.onNewNodeChoiceTwo.bind(this);
-
-  }
-
-  handleChoiceOneChange(event) {
-    this.setState({
-      choiceOne: event.target.value
-    })
-
-  }
-
-  handleChoiceTwoChange(event) {
-    this.setState({
-      choiceTwo: event.target.value
-    })
-
-  }
-
-  handleLinkChange(event) {
-    this.setState({
-      link: event.target.value
-    })
-
+    this.handleParentClick = this.handleParentClick.bind(this);
+    this.handleChoiceClick = this.handleChoiceClick.bind(this);
   }
 
   handleSubmit(event) {
-    console.log('currentNode', currentNode);
-    currentNode.video = this.state.link
-    currentNode.choiceOneText = this.state.choiceOne
-    currentNode.choiceTwoText = this.state.choiceTwo
-    console.log('update', node)
-
-
-      this.setState({
-        choiceOne: '',
-        choiceTwo: '',
-        link: ''
-      });
     event.preventDefault();
+    console.log('Submitted!')
   }
 
-  onGoBack(event) {
+  handleParentClick(event) {
+    console.log('You Clicked!')
   }
 
-  onNewNodeChoiceOne(event) {
-    let temp = this.state.currentNode.addChild(new Node())
-    this.setState({
-      parent: this.state.currentNode,
-      currentNode: temp
-    }, function() {
-      console.log('currentNode', node)
-    })
-
-    event.preventDefault();
-  }
-
-  onNewNodeChoiceTwo(event) {
-    let kid = currentNode.addChild()
-    this.setState({
-      currentNode: kid
-    })
-    event.preventDefault();
+  handleChoiceClick(event) {
+    console.log('You Clicked Here Too!')
   }
 
   render() {
@@ -112,14 +44,35 @@ class VideoCreator extends React.Component {
             spacing={24}
             style={{padding: 24, borderBottom: '1px solid black'}}
             direction="column"
+            onClick={this.handleParentClick}
           >
             <Typography
-              variant="h5"
-              component="h3"
+              variant="h6"
+              component="h6"
               align="center"
             >
-              <Grid item s={12}>
+              <Grid item xs={12} >
                 Parent Story Node
+              </Grid>
+              <Grid item xs={12}>
+                Link
+              </Grid>
+              <Grid item xs={12}>
+                Link Goes Here
+              </Grid>
+              <Grid item xs={6} sm container direction="row" spacing={16}>
+                <Grid item xs={6}>
+                  Choice One
+                </Grid>
+                <Grid item xs={6}>
+                  Choice Two
+                </Grid>
+                <Grid item xs={6}>
+                Choice One Here
+              </Grid>
+              <Grid item xs={6}>
+                Choice Two Here
+              </Grid>
               </Grid>
             </Typography>
           </Grid>
@@ -177,6 +130,15 @@ class VideoCreator extends React.Component {
                       variant="filled"
                     />
                   </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                    >
+                      Submit Node
+                    </Button>
+                  </Grid>
                 </Grid>
 
               </form>
@@ -192,116 +154,80 @@ class VideoCreator extends React.Component {
             direction="column"
           >
             <Typography
-              variant="h5"
-              component="h3"
+              variant="h6"
+              component="h6"
               align="center"
             >
               <Grid item spacing={16}>
                 Choices
               </Grid>
+              <Grid
+                container
+                spacing={24}
+                style={{padding: 24}}
+                direction="column"
+              >
+                <Grid item xs={12} xs container direction="row">
+                  <Grid item xs={6}>
+                    One
+                  </Grid>
+                  <Grid item xs={6}>
+                    Two
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} xs container direction="row">
+                  <Grid item xs={6}>
+                    Link
+                  </Grid>
+                  <Grid item xs={6}>
+                    Link
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} xs container direction="row">
+                  <Grid item xs={6}>
+                    Link Here
+                  </Grid>
+                  <Grid item xs={6}>
+                    Link Here
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} xs container direction="row">
+                  <Grid item xs={3}>
+                    Choice One
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice Two
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice One
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice Two
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} xs container direction="row">
+                  <Grid item xs={3}>
+                    Choice One Here
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice Two Here
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice One Here
+                  </Grid>
+                  <Grid item xs={3}>
+                    Choice Two Here
+                  </Grid>
+                </Grid>
 
+              </Grid>
 
             </Typography>
           </Grid>
 
 
         </Paper>
-        {/* <form onSubmit={this.handleSubmit}>
-          <Paper elevation={1}>
-            <Grid container spacing={24} style={{padding: 24}}>
-              <Typography variant="h5" component="h3">
-                Current Node
-              </Typography>
-              <Grid
-                container
-                item sm={12}
-                direction="column"
-                justify="center"
-                alignItems="center"
-              >
 
-                <TextField style={{padding:24}}
-                  id="Link"
-                  type="url"
-                  placeholder="Link"
-                  value={this.state.link}
-                  onChange={this.handleLinkChange}
-                  margin="normal"
-                  variant="filled"
-                />
-              </Grid>
-
-              <Grid item sm={6}>
-
-                <TextField
-                  style={{padding:24}}
-                  id="choiceOne"
-                  type="text"
-                  placeholder="Choice One Button Text"
-                  value={this.state.choiceOne}
-                  onChange={this.handleChoiceOneChange}
-                  margin="normal"
-                  variant="filled"
-                />
-
-
-              </Grid>
-
-              <Grid item sm={6}>
-
-                <TextField style={{padding:24}}
-                  id="choiceTwo"
-                  type="text"
-                  placeholder="Choice Two Button Text"
-                  value={this.state.choiceTwo}
-                  onChange={this.handleChoiceTwoChange}
-                  margin="normal"
-                  variant="filled"
-                />
-
-
-              <Grid item sm={6}>
-                <Button
-                variant="contained"
-                color="primary"
-                onClick={this.onNewNodeChoiceOne}
-                >
-                  New Node for Choice One
-                </Button>
-
-                <Button
-                variant="contained"
-                color="primary"
-                onClick={this.onNewNodeChoiceTwo}
-                >
-                  New Node for Choice Two
-                </Button>
-              </Grid>
-
-              </Grid>
-              <Grid item sm={6}>
-                <Button
-                variant="contained"
-                color="primary"
-                onClick={this.onGoBack}
-                >
-                  Go Back
-                </Button>
-
-                <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                >
-                  Submit Node
-                </Button>
-              </Grid>
-              <Typography variant="h5" component="h3">
-              Parent Node
-            </Typography>
-            </Grid>
-          </Paper>
-        </form> */}
       </div>
     )
   }
